d6347e013b6130c9393f65cae2cc1a12ce1672a8
Brandon LeBlanc <brandon@leblanc.codes>
2021-12-06 22:39:46 -0800

    day 7

---
+++ src/y2021/d07/mod.rs
@@ -0,0 +1,30 @@
+pub mod part1;
+pub mod part2;
+
+#[derive(derive_more::IntoIterator)]
+#[into_iterator(ref)]
+struct Ocean(Vec<isize>);
+
+impl Ocean {
+    pub fn solve<F>(self, mut cost: F) -> isize
+    where
+        F: FnMut(isize, isize) -> isize,
+    {
+        self.range()
+            .map(|a| (&self).into_iter().map(|b| (cost)(a, *b)).sum())
+            .min()
+            .unwrap_or_default()
+    }
+
+    pub fn range(&self) -> std::ops::RangeInclusive<isize> {
+        self.min()..=self.max()
+    }
+
+    pub fn min(&self) -> isize {
+        self.into_iter().min().unwrap_or(&0).to_owned()
+    }
+
+    pub fn max(&self) -> isize {
+        self.into_iter().max().unwrap_or(&0).to_owned()
+    }
+}
---
+++ src/y2021/d07/part2.rs
@@ -0,0 +1,78 @@
+/*
+    --- Part Two ---
+    The crabs don't seem interested in your proposed solution. Perhaps you misunderstand
+    crab engineering?
+
+    As it turns out, crab submarine engines don't burn fuel at a constant rate. Instead,
+    each change of 1 step in horizontal position costs 1 more unit of fuel than the
+    last: the first step costs 1, the second step costs 2, the third step costs 3, and
+    so on.
+
+    As each crab moves, moving further becomes more expensive. This changes the best
+    horizontal position to align them all on; in the example above, this becomes 5:
+
+        Move from 16 to 5: 66 fuel
+        Move from 1 to 5: 10 fuel
+        Move from 2 to 5: 6 fuel
+        Move from 0 to 5: 15 fuel
+        Move from 4 to 5: 1 fuel
+        Move from 2 to 5: 6 fuel
+        Move from 7 to 5: 3 fuel
+        Move from 1 to 5: 10 fuel
+        Move from 2 to 5: 6 fuel
+        Move from 14 to 5: 45 fuel
+
+    This costs a total of 168 fuel. This is the new cheapest possible outcome; the old
+    alignment position (2) now costs 206 fuel instead.
+
+    Determine the horizontal position that the crabs can align to using the least fuel
+    possible so they can make you an escape route! How much fuel must they spend to
+    align to that position?
+*/
+
+use std::{convert::Infallible, io::BufRead};
+
+use crate::{Error, Problem, Solution};
+
+use super::Ocean;
+
+#[derive(derive_more::IntoIterator)]
+#[into_iterator(ref)]
+struct Answer(Ocean);
+
+impl<R: BufRead> TryFrom<Problem<R>> for Answer {
+    type Error = Error;
+
+    fn try_from(mut value: Problem<R>) -> Result<Self, Self::Error> {
+        Ok(Self(Ocean(value.expect_map_line(",", str::parse)?)))
+    }
+}
+
+impl Solution for Answer {
+    type Err = Infallible;
+
+    fn try_into_answer(self) -> Result<isize, Self::Err> {
+        Ok(self.0.solve(|a, b| {
+            let d = (b - a).abs();
+            (d * (d + 1)) / 2
+        }))
+    }
+}
+
+mod test {
+    #[test]
+    fn test_example() {
+        assert_eq!(
+            crate::solve::<super::Answer>(include_str!("inputs/example")).unwrap(),
+            168
+        )
+    }
+
+    #[test]
+    fn test_live() {
+        assert_eq!(
+            crate::solve::<super::Answer>(include_str!("inputs/live")).unwrap(),
+            96361606
+        )
+    }
+}
